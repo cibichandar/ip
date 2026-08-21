ee83885137184ba85a5da35c91295a015526c45c
cibichandar <cibichandar2004@gmail.com>
2026-08-21 14:46:09 +0800

    Implemented chatbot initial greeting flow

--- src/main/java/Fein.java
+++ src/main/java/Fein.java
@@ -1,5 +1,6 @@
 public class Fein {
     public static void main(String[] args) {
+        String separator = "_".repeat(100);
         String banner = "oooooooooooo           o8o                     \n"
                 + "`888'     `8           `\"'                     \n"
                 + " 888          .ooooo.  oooo  ooo. .oo.         \n"
@@ -7,6 +8,13 @@ public class Fein {
                 + " 888    \"    888ooo888  888   888   888        \n"
                 + " 888         888    .o  888   888   888        \n"
                 + "o888o        `Y8bod8P' o888o o888o o888o       \n";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Fein.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
