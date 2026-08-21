c849d570bbd558a4cbce8eebfdbd44cd9da76f48
cibichandar <cibichandar2004@gmail.com>
2026-08-21 14:39:17 +0800

    Renamed chatbot to Fein and updated documentation

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Fein User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Fein.java
@@ -0,0 +1,12 @@
+public class Fein {
+    public static void main(String[] args) {
+        String banner = "oooooooooooo           o8o                     \n"
+                + "`888'     `8           `\"'                     \n"
+                + " 888          .ooooo.  oooo  ooo. .oo.         \n"
+                + " 888oooo8    d88' `88b `888  `888P\"Y88b        \n"
+                + " 888    \"    888ooo888  888   888   888        \n"
+                + " 888         888    .o  888   888   888        \n"
+                + "o888o        `Y8bod8P' o888o o888o o888o       \n";
+        System.out.println(banner);
+    }
+}
