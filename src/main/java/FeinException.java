9df1f5eae80d31d0b29d1dc1d854750476b4f0fd
cibichandar <cibichandar2004@gmail.com>
2026-08-27 19:34:39 +0800

    Handle invalid commands with FeinException

---
+++ src/main/java/FeinException.java
@@ -0,0 +1,7 @@
+/** Represents an input error that Fein can explain to the user. */
+public class FeinException extends Exception {
+    /** Creates an input error with the given user-facing message. */
+    public FeinException(String message) {
+        super(message);
+    }
+}
